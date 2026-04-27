32058e32e3a829a0b819739cee6b99a79e5af3fd
Rajesh Samal <rajeshsamal745@gmail.com>
2026-04-27 10:25:18 +0000

    Added Day_02 Arguments.sh

--- Day_02/Arguments.sh
+++ Day_02/Arguments.sh
@@ -2,3 +2,12 @@
 
 #Print first argument
 echo "First argument: $1"
+
+#Add two numbers from arguments
+sum=$(( $1 + $2 ))
+echo "Sum: $sum"
+
+#Loop through all arguments
+for arg in "$@"; do
+	echo "Argument: $arg"
+done
